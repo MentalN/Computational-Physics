3c8f804e752e42d1b7c6dd90ee738b0151332535
Nawaf Abdullah <nawaf97k@gmail.com>
2018-03-19 19:55:03 -0400

    Wrapped the functionality in a class Bicycle

--- ProjectileMotion/BicylceCalc.py
+++ ProjectileMotion/BicylceCalc.py
@@ -16,41 +16,40 @@ import matplotlib.pyplot as plt
 # A   = Frontal surface area
 
 
-#   Calculates the velocity of a Bicycle vs Time with no drag
-def bicycle_calc_no_drag(P, m, dt, N, vo):
-    v = [vo]
+class Bicycle:
+    v = []
     t = [0]
-    for i in range(N):
-        v.append(v[i] + (P/(m*v[i]))*dt)
-        t.append(t[i] + dt)
-    cap = "Bicycle Velocity vs Time (No drag)"
-#   returning time array, velocity array, and a string for the plot title
-    return t, v, cap
+    cap = 0
+
+#   Calculates the velocity of a Bicycle vs Time with no drag
+    def bicycle_no_drag(self, P, m, dt, N, vo):
+        self.v = [vo]
+        for i in range(N):
+            self.v.append(self.v[i] + (P/(m*self.v[i]))*dt)
+            self.t.append(self.t[i] + dt)
+        self.cap = "Bicycle Velocity vs Time (No drag)"
+#       returning time array, velocity array, and a string for the plot title
+        return self.t, self.v
 
 
 #   Calculates the velocity of a Bicycle vs Time with drag
-def bicycle_calc_drag(P, m, dt, N, vo, C, rho, A):
-    v = [vo]
-    t = [0]
-    for i in range(N):
-        D = (C*rho*A*v[i]*v[i]/(2*m))*dt
-        v.append(v[i] + (P/(m*v[i]))*dt - D)
-        t.append(t[i] + dt)
-    cap = "Bicycle Velocity vs Time (with drag)"
-#   returning time array, velocity array, and a string for the plot title
-    return t, v, cap
+    def bicycle_drag(self, P, m, dt, N, vo, C, rho, A):
+        self.v = [vo]
+        for i in range(N):
+            D = (C*rho*A*self.v[i]*self.v[i]/(2*m))*dt
+            self.v.append(self.v[i] + (P/(m*self.v[i]))*dt - D)
+            self.t.append(self.t[i] + dt)
+            self.cap = "Bicycle Velocity vs Time (with drag)"
+#       returning time array, velocity array, and a string for the plot title
+        return self.t, self.v
 
 
 #   Plots whatever velocity and time arrays passed to it
-def bicycle_plot(t, v, cap):
-    fig = plt.figure()
-    fig.suptitle(cap)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Velocity (m/s)')
-    plt.plot(t, v)
-    plt.show()
+    def bicycle_plot(self):
+        fig = plt.figure()
+        fig.suptitle(self.cap)
+        plt.xlabel('Time (s)')
+        plt.ylabel('Velocity (m/s)')
+        plt.plot(self.t, self.v)
+        plt.show()
 
-
-#   tp, vp, title = bicycle_calc_no_drag(P=400, m=70, dt=0.1, N=2000, vo=4)
-#   tp, vp, title = bicycle_calc_drag(P=400, m=70, dt=0.1, N=2000, vo=4, C=0.5, rho=1.229, A=0.33)
-#   bicycle_plot(tp, vp, title)
